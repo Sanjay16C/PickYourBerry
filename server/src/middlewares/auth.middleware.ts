1a59bdd903ab8f605d579f9db7b1baa81f14a032
Sanjay16C <sanjaychandrasekar16@gmail.com>
2026-09-14 15:13:45 +0530

    auth middleware

---
+++ server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = async(req,res)=>{
+    try {
+        const tokenHeader = req.headers.authorization;
+        if(!tokenHeader) return res.status(401).json({
+            message : "No Token"
+        })
+        const token = tokenHeader.split(" ")[1];
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+
+    } catch (error) {
+        res.status(401).json({
+            message : "Invalid Token"
+        })
+    }
+}
